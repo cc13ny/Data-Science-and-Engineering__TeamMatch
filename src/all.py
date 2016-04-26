8b2df8f9324bc12785931595ecc25689a81cdb3e
cyc <cc13ny@users.noreply.github.com>
2016-04-26 10:14:46 -0400

    Update all.py

--- src/all.py
+++ src/all.py
@@ -1,3 +1,40 @@
 class Member(object):
+    def __init__(self, uid):
+        self.uid = uid
+        self.score = 100
+    
+    def adjust_score(self, val):
+        self.score += val
+    
+''' 
+class Member(object):
+    def __init__(self, uid, name, email, passport):
+        self.uid = uid
+        self.name = name
+        self.email = email
+        self.passort = passport
+        self.scores =
+    
+    def know_achieve_typelist(self):
+        pass
+    
+    def know_my_achievelist(self):
+        pass
+    
+    def claim_achieve(self):
+        pass
+
+class Admin(object):
     def __init__(self):
-        
+        self.playerlist = []
+
+class Score(object):
+    def __init__(self):
+        pass
+    
+class Object(object):
+    def __init__(self):
+        self.type
+
+'''
+  
